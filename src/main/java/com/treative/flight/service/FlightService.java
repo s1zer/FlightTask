6205eec2ec15bbc5f9a627c36cccbfd3a79c1272
Michal <wierzbicki.m1995@gmail.com>
2019-06-19 15:19:13 +0200

    FlightService finished.

---
+++ src/main/java/com/treative/flight/service/FlightService.java
@@ -0,0 +1,44 @@
+package com.treative.flight.service;
+
+import com.treative.flight.components.dto.FlightDto;
+import com.treative.flight.components.mapper.FlightMapper;
+import com.treative.flight.components.model.Flight;
+import com.treative.flight.repository.FlightRepository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class FlightService {
+
+    private FlightRepository flightRepository;
+    private FlightMapper flightMapper;
+
+    public FlightService(FlightRepository flightRepository, FlightMapper flightMapper) {
+        this.flightRepository = flightRepository;
+        this.flightMapper = flightMapper;
+    }
+
+    public List<FlightDto> findAllFlights() {
+        return flightRepository.findAll()
+                .stream()
+                .map(flightMapper::toDto)
+                .collect(Collectors.toList());
+    }
+
+    public Optional<FlightDto> findFlightById(Long id) {
+        return flightRepository.findById(id)
+                .map(flightMapper::toDto);
+    }
+
+    public FlightDto saveFlight(FlightDto flightDto) {
+        Flight savedFlight = flightRepository.save(flightMapper.toEntity(flightDto));
+        return flightMapper.toDto(savedFlight);
+    }
+
+    public void deleteFlightById(Long id) {
+        Optional<Flight> flightToRemove = flightRepository.findById(id);
+        flightToRemove.ifPresent(t -> flightRepository.delete(t));
+    }
+
+}
